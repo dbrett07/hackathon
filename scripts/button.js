6d142ced63fabf233149cda44aa8ccb8a00020fc
KorziChupacabra <korzurei.twitch@outlook.com>
2025-10-18 13:38:51 +0100

    trying to add a button

---
+++ scripts/button.js
@@ -0,0 +1,9 @@
+var x = document.createElement("BUTTON");   
+x.innerHTML = "Click Me";                  
+x.id = "myButton"             
+document.body.appendChild(x);
+
+function buttonClicked() {
+    
+
+}
